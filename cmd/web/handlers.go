36ffa9d2dbc85b3c5bf0e709f50992f3de6fa28a
Calvin Webster <calweb@gmail.com>
2021-03-29 16:15:31 -0400

    chapter 7, Restful routing, new web framework pat

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,10 +9,6 @@ import (
 	"calvin.nz/snippetbox/pkg/models"
 )
 func (app *application) home(w http.ResponseWriter, r *http.Request) { 
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 
 	s, err := app.snippets.Latest()
 	if err != nil {
@@ -26,7 +22,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) { 
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -48,12 +44,11 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) { 
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost) 
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
 
 	title := "Old Haiku - Calvin"
 	content := "Whispering winds blow\nThrough greyish lines of sorrow,\nsomething, something!\n\n- Calvin Webster"
@@ -64,5 +59,5 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
